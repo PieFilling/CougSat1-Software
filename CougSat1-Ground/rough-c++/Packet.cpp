fe4b9824a0bbecc79b0ca4e69a5b9540206d6b80
Adrian Lemus <adrianlemus96@gmail.com>
2018-06-09 23:00:03 -0700

    Created template for the foundation of onboard packets
    Generated foundation for Packets. Created virtual class which will act as frame for all future packets. Will be using factory design pattern to generate packets later.

---
+++ CougSat1-Ground/rough-c++/Packet.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "Packet.hpp"
+
+Packet::Packet()
+{
+    // don't do shit nigga
+}
+
+
+// Standard Packet function declarations
+void InfoPacket::printPacket(){
+
+}
+
+void InfoPacket::init(){
+    
+}
+
+PacketTypes InfoPacket::getType(){
+    
+}
+
+int InfoPacket::getSize(){
+    
+}
+
+
+// Standard Packet function declarations
+void StandardPacket::printPacket(){
+
+}
+
+void StandardPacket::init(){
+    
+}
+
+PacketTypes StandardPacket::getType(){
+    
+}
+
+int StandardPacket::getSize(){
+    
+}
+
